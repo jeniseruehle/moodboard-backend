b031cfaee83b2b6d7a1cb078e934df8005dcab20
Jenise Ruehle <jeniseruehle@gmail.com>
2021-08-03 15:49:15 -0500

    updated routes + created seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :moods
-  resources :boards
+  resources :moods, only: [:index, :create, :update, :destroy]
+  resources :boards, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Board.destroy_all
+Mood.destroy_all
+
+engagement = Board.create(title: "Engagement")
+dress = Board.create(title: "Dress")
+flowers = Board.create(title: "Flowers")
+
+poses = Mood.create(name: "Poses", desc: "Couples at the beach", image: "https://www.judithsfreshlook.com/wp-content/uploads/2018/03/Fresh-Look-Photography_Virginia-Beach_Engagement-Session_0010.jpg", board_id: engagement.id)
+mermaid = Mood.create(name: "The Dress", desc: "Stella York", image: "https://www.essensedesigns.com/wp-content/uploads/2018/10/6750-1.jpg", board_id: dress.id)
+bouquet = Mood.create(name: "My Bouquet", desc: "Bohemian Style Bouquet", image: "https://cdn.shopify.com/s/files/1/1334/4597/files/BohemianBouquet_DIY_5-11_pic1_1024x1024_ea5a5055-9c73-49b8-9e0a-e837a8406087_1024x1024.jpg?v=1525963116", board_id: flowers.id)
