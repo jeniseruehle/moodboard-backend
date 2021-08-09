8a4b39793fc8771846f77f95452c1ffa572c7ed0
Jenise Ruehle <jeniseruehle@gmail.com>
2021-08-09 16:59:48 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,11 @@ Mood.destroy_all
 engagement = Board.create(title: "Engagement")
 dress = Board.create(title: "Dress")
 flowers = Board.create(title: "Flowers")
+reception = Board.create(title: "Reception")
+ceremony = Board.create(title: "Ceremony")
+other = Board.create(title: "Other")
 
 poses = Mood.create(name: "Poses", desc: "Couples at the beach", image: "https://www.judithsfreshlook.com/wp-content/uploads/2018/03/Fresh-Look-Photography_Virginia-Beach_Engagement-Session_0010.jpg", board_id: engagement.id)
 mermaid = Mood.create(name: "The Dress", desc: "Stella York", image: "https://www.essensedesigns.com/wp-content/uploads/2018/10/6750-1.jpg", board_id: dress.id)
 bouquet = Mood.create(name: "My Bouquet", desc: "Bohemian Style Bouquet", image: "https://cdn.shopify.com/s/files/1/1334/4597/files/BohemianBouquet_DIY_5-11_pic1_1024x1024_ea5a5055-9c73-49b8-9e0a-e837a8406087_1024x1024.jpg?v=1525963116", board_id: flowers.id)
+centerpieces = Mood.create(name: "Centerpieces", desc: "Floral + Candles", image: "https://cdn0.weddingwire.ca/img_e_12279/2/2/7/9/r10_2x_img-4523_50_12279.jpg", board_id: reception.id)
